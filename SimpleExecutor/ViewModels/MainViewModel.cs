bd19dab893699abc1f5226017950358748ceac81
Walperr <vladbann37@yandex.ru>
2023-11-19 23:06:42 +0700

    added predefined sqrt and abs functions

--- SimpleExecutor/ViewModels/MainViewModel.cs
+++ SimpleExecutor/ViewModels/MainViewModel.cs
@@ -18,19 +18,27 @@ public class MainViewModel : ViewModelBase
 
     public MainViewModel()
     {
-        FunctionBase printFunction = new Function("print", new[] {new Variable("text", typeof(object))}, args =>
+        var printFunction = new Function("print", new[] {new Variable("text", typeof(object))}, args =>
         {
             Output += args.First().ToString();
         });
         
-        FunctionBase printLineFunction = new Function("printLine", new[] {new Variable("text", typeof(object))}, args =>
+        var printLineFunction = new Function("printLine", new[] {new Variable("text", typeof(object))}, args =>
         {
             Output += "\n" + args.First();
         });
 
+        var sqrtFunction = new Function<double>("sqrt", new[] {new Variable("d", typeof(double))},
+            args => Math.Sqrt((double) args.First()));
+
+        var absFunction = new Function<double>("abs", new[] {new Variable("d", typeof(double))},
+            args => Math.Abs((double) args.First()));
+
         _interpreter = InterpreterBuilder.CreateBuilder()
             .WithPredefinedFunction(printFunction)
             .WithPredefinedFunction(printLineFunction)
+            .WithPredefinedFunction(sqrtFunction)
+            .WithPredefinedFunction(absFunction)
             .Build();
     }
 
@@ -46,7 +54,7 @@ public class MainViewModel : ViewModelBase
         set => this.RaiseAndSetIfChanged(ref _output, value);
     }
 
-    public ICommand? Interpret => ReactiveCommand.Create( async () =>
+    public ICommand? Interpret => ReactiveCommand.Create(() =>
     {
         Output = string.Empty;
         
@@ -58,7 +66,7 @@ public class MainViewModel : ViewModelBase
             return;
         }
 
-        var value = await Task.Run(() => _interpreter.Interpret());
+        var value = _interpreter.Interpret();
 
         if (value.IsError)
             Output += "\n Error:\n" + value.Error.Message + value.Error.Range;
